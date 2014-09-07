d34c81d7fd8239ced0c74fcc0b6c48c153c67359
dmh <dmh@unidata.ucar.edu>
2014-09-07 12:07:47 -0600

    Fix cmake to support bzip2

--- cf.cmake
+++ cf.cmake
@@ -7,4 +7,3 @@ HDF5="-DHDF5_LIB=${UL}/lib/libhdf5.so -DHDF5_HL_LIB=${UL}/lib/libhdf5_hl.so -DHD
 cmake -DCMAKE_INSTALL_PREFIX=${UL} -DCMAKE_PREFIX_PATH="$PPATH" ${HDF5} ..
 cmake --build .
 make test
-
