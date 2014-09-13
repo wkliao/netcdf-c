1c03074cdc4801c37e31ab658ae1fef17e946a5a
dmh <dmh@unidata.ucar.edu>
2014-09-13 15:54:10 -0600

    Added the _DWITH_COMPRESSION=<list of compression algorithms> flag to cmake. zip is always supported, and bzip2 can optionally be supported with -DWITH-COMPRESSION=bzip2

--- cf.cmake
+++ cf.cmake
@@ -1,9 +1,11 @@
 rm -fr build
 mkdir build
 cd build
+BZ=-DWITH_COMPRESSION=bzip2
 UL=/usr/local
 PPATH="$UL"
+#NONC4="-DENABLE_NETCDF_4=OFF"
 HDF5="-DHDF5_LIB=${UL}/lib/libhdf5.so -DHDF5_HL_LIB=${UL}/lib/libhdf5_hl.so -DHDF5_INCLUDE_DIR=${UL}/include"
-cmake -DCMAKE_INSTALL_PREFIX=${UL} -DCMAKE_PREFIX_PATH="$PPATH" ${HDF5} ..
+cmake -DCMAKE_INSTALL_PREFIX=${UL} -DCMAKE_PREFIX_PATH="$PPATH" ${HDF5} ${NONC4} ${BZ} ..
 cmake --build .
 make test
